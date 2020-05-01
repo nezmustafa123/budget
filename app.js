862ca0627a8a48bb8937c2697824f8c8a82ebbc0
nezmustafa123 <nezmustafa123@gmail.com>
2020-05-01 22:19:39 +0100

    added percentage caluculator method for items

--- app.js
+++ app.js
@@ -228,7 +228,8 @@ var UIController = (function(){
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
 	};
     
 	return {
@@ -314,6 +315,33 @@ var UIController = (function(){
                  document.querySelector(DOMstrings.percentageLabel).textContent = '---'
              }
          },
+        
+        //recieve percentage array in app controller
+          displayPercentages: function(percentages) {
+              var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+              
+              
+              var nodeListForEach = function(list, callback) {
+                  for (var i = 0; i < list.length; i ++) {
+                      callback(list[i], i);
+                  }
+              };
+              
+              nodeListForEach(fields, function(current, index){
+                  
+                  //do stuff
+                  
+                 if(percentages[index] > 0){
+                current.textContent = percentages[index] + '%';
+              } else {
+                  current.textContent = '---';
+              }
+             });
+          },
+         
+        
+        
+        
 		  getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -367,7 +395,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         //2. Read percentages from the budget controller
         var percentages = budgetCtrl.getPercentages();
         //3. Update the UI with the new percentages
-        console.log(percentages);
+//        console.log(percentages);
+        
+        UICtrl.displayPercentages(percentages);
     };
     
 	var ctrlAddItem = function() {
